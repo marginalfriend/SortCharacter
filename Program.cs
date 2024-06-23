553f6ad8f5c1d57d2b6370babb819569b1aa7fb8
marginalfriend <abdullahpsg@gmail.com>
2024-06-23 10:55:18 +0700

    [INITIAL] initial submission

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortCharacter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input one line of words (S): ");
+            string input = Console.ReadLine();
+
+            string vowels = "aeiou";
+            StringBuilder vowelCharacters = new StringBuilder();
+            StringBuilder consonantCharacters = new StringBuilder();
+
+            foreach (char c in input.ToLower())
+            {
+                if (char.IsWhiteSpace(c)) continue;
+
+                if (vowels.Contains(c))
+                {
+                    vowelCharacters.Append(c);
+                }
+                else if (char.IsLetter(c))
+                {
+                    consonantCharacters.Append(c);
+                }
+            }
+
+            Console.WriteLine("Vowel Characters: " + vowelCharacters.ToString());
+            Console.WriteLine("Consonant Characters: " + consonantCharacters.ToString());
+        }
+    }
+}
